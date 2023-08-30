e81ed2054e5e1c2d2bb1e57945fd9f35d5f4ad05
Andrii Yevstratiev <andrii_yevstratiev@epam.com>
2023-08-30 09:50:19 +0300

    added configuration for deploying on tomcat

--- src/main/java/com/example/cicd/CiCdApplication.java
+++ src/main/java/com/example/cicd/CiCdApplication.java
@@ -2,9 +2,10 @@ package com.example.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CiCdApplication {
+public class CiCdApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CiCdApplication.class, args);
